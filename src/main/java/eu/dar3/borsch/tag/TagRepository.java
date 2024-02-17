8b8ae92d698a81a2e75be6e3a7184e533cab4745
Svyatoslav Shypka <svyat200909@gmail.com>
2024-02-17 18:51:13 +0100

    TagRepository

---
+++ src/main/java/eu/dar3/borsch/tag/TagRepository.java
@@ -0,0 +1,10 @@
+package eu.dar3.borsch.tag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TagRepository extends JpaRepository<Tag, UUID> {
+    List<Tag> findAllByTitle(String title);
+}
