97d48024d64ec69547194444269a40d2d188bf86
Svyatoslav Shypka <svyat200909@gmail.com>
2024-02-17 11:50:59 +0100

    Start

---
+++ src/test/java/eu/dar3/borsch/BorschApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.dar3.borsch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorschApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
