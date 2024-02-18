79f0cbecf3134f3d9a1e537eee70db39ce94797e
Svyatoslav Shypka <svyat200909@gmail.com>
2024-02-18 12:18:11 +0100

    InfoMessages

---
+++ src/main/java/eu/dar3/borsch/authentification/AuthController.java
@@ -0,0 +1,49 @@
+package eu.dar3.borsch.authentification;
+
+import eu.dar3.borsch.errors.ErrorMessages;
+import eu.dar3.borsch.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.NoSuchElementException;
+
+@RequiredArgsConstructor
+@Controller
+public class AuthController {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "user/login";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("errorsMessages") ErrorMessages errorsMessages,
+                               @ModelAttribute("infoMessages") InfoMessages infoMessages,
+                               @RequestParam(value = "username") String username,
+                               @RequestParam(value = "password") String password,
+                               @RequestParam(value = "nickname") String nickname) {
+        try {
+            userService.findUserByName(username);
+            errorsMessages.addError("Користувач вже зареєстрований. Ви можете увійти.");
+            return "user/login";
+        } catch (NoSuchElementException e) {
+            userService.createNewUser(username, password, nickname);
+            infoMessages.addMessage("Ви успішно зареєструвалися. Можете увійти.");
+            return "user/login";
+        }
+    }
+
+}
---
+++ src/main/java/eu/dar3/borsch/errors/InfoMessages.java
@@ -0,0 +1,17 @@
+package eu.dar3.borsch.errors;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class InfoMessages {
+
+    private List<String> messages = new ArrayList<>();
+
+    public void addMessage(String message) {
+        messages.add(message);
+    }
+
+}
