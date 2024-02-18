67a14512544eb813193b59fd031b47222edccd7d
Svyatoslav Shypka <svyat200909@gmail.com>
2024-02-18 12:13:14 +0100

    AppController

---
+++ src/main/java/eu/dar3/borsch/web/AppController.java
@@ -0,0 +1,35 @@
+package eu.dar3.borsch.web;
+
+import eu.dar3.borsch.user.UserOptionsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping("/")
+@RequiredArgsConstructor
+@Controller
+public class AppController {
+
+    private final UserOptionsService userOptionsService;
+
+    @GetMapping("/")
+    public String getMainPage() {
+        return "redirect:recipe/list";
+    }
+
+    @GetMapping("/contacts")
+    public ModelAndView getContactsPage() {
+        ModelAndView result = new ModelAndView("pages/contacts");
+        result.addObject("options", userOptionsService.getOptions());
+        return result;
+    }
+
+    @GetMapping("/about")
+    public ModelAndView getAboutPage() {
+        ModelAndView result = new ModelAndView("pages/about");
+        result.addObject("options", userOptionsService.getOptions());
+        return result;
+    }
+}
