933c541479e48f00ec5678c5e21f8bc889276b2f
Svyatoslav Shypka <svyat200909@gmail.com>
2024-02-18 13:02:11 +0100

    application.properties logback.xml

---
+++ src/main/java/eu/dar3/borsch/security/WebSecurityConfig.java
@@ -0,0 +1,74 @@
+package eu.dar3.borsch.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().disable().csrf().disable()
+                .authorizeHttpRequests(requests -> {
+                            requests
+                                    .requestMatchers(
+                                            "/about",
+                                            "/contacts",
+                                            "/login",
+                                            "/img/**",
+                                            "/css/**",
+                                            "/recipe/share/**",
+                                            "/error/**",
+                                            "/register/**"
+                                    )
+                                    .permitAll();
+                            requests
+                                    .requestMatchers("/").permitAll()
+                                    .anyRequest()
+                                    .authenticated();
+                        }
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .formLogin(a -> a.loginPage("/login"))
+                .formLogin(a -> a.defaultSuccessUrl("/", true).permitAll())
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().passwordEncoder(passwordEncoder())
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username, password, enabled from access.users where username=?")
+                .authoritiesByUsernameQuery("select username, role from access.users where username=?");
+    }
+}
