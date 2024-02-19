30caaf73b796d0db349256d8d577e6445b0e8744
Svyatoslav Shypka <svyat200909@gmail.com>
2024-02-19 09:47:04 +0100

    SQL scripts

--- src/main/resources/db/migration/V2024.02.18.001__create-schema-access.sql
+++ src/main/resources/db/migration/V2024.02.18.001__create-schema-access.sql
@@ -1,27 +1,28 @@
-create schema if not exists access;
+CREATE SCHEMA IF NOT EXISTS access;
 
-create table access.gender(
+CREATE TABLE IF NOT EXISTS access.gender(
                               gender_id    numeric(1)    primary key,
                               name_ukr     varchar(50)   not null,
                               name_eng     varchar(50)   not null
 );
 
-create unique index ix_gender_gender_id on access.gender (gender_id);
+CREATE UNIQUE INDEX IF NOT EXISTS ix_gender_gender_id on access.gender (gender_id);
 
-create table access.users(
-                             user_id        uuid          primary key,
-                             username       varchar(100)   not null,
-                             password       varchar(500)  not null,
-                             role           varchar(32)   not NULL,
-                             enabled        boolean       not null,
-                             nickname       varchar(50)  not null,
-                             birthday       date,
-                             gender_id      integer       not null default 0,
-                             created_date   timestamp,
-                             updated_date   timestamp,
-                             friendgroup_id UUID            nullable,
+CREATE TABLE IF NOT EXISTS access.users(
+                             user_id         UUID           primary key,
+                             username        varchar(100)   not null,
+                             password        varchar(500)   not null,
+                             role            varchar(32)    not NULL,
+                             enabled         boolean        not null,
+                             nickname        varchar(50)    not null,
+                             birthday        date,
+                             gender_id       integer        not null default 0,
+                             created_date    timestamp,
+                             updated_date    timestamp,
+                             friendgroup_id  UUID           default null,
+                             full_width_page boolean        default false,
                              constraint fk_users_gender foreign key(gender_id) references access.gender(gender_id)
 );
-create unique index ix_users_user_id   on access.users (user_id);
-create unique index ix_users_username  on access.users (username);
-create        index ix_users_gender_id on access.users (gender_id);
+CREATE UNIQUE INDEX IF NOT EXISTS ix_users_user_id   on access.users (user_id);
+CREATE UNIQUE INDEX IF NOT EXISTS ix_users_username  on access.users (username);
+CREATE INDEX IF NOT EXISTS ix_users_gender_id on access.users (gender_id);
--- src/main/resources/db/migration/V2024.02.18.004__insert-recipe-access-type.sql
+++ src/main/resources/db/migration/V2024.02.18.004__insert-recipe-access-type.sql
@@ -1,3 +1,3 @@
-INSERT INTO recipes.note_access_type (RECIPE_ACCESS_TYPE, NAME_UKR, NAME_ENG)
-values ('private', 'Приватний', 'Private'),
+INSERT INTO recipes.recipe_access_type (RECIPE_ACCESS_TYPE, NAME_UKR, NAME_ENG)
+VALUES ('private', 'Приватний', 'Private'),
        ('public',  'Публічний', 'Public');
