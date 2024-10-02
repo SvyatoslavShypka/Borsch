6cf4c28ab145abcb576282c78dfa5747297fe595
Svyatoslav Shypka <svyat200909@gmail.com>
2024-10-02 21:27:32 +0200

    ver. 1.0.4 cicd_154 commented DockerFile

--- run.sh
+++ run.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
+#Indicates the script will be run using the Bourne shell (sh).
+#Disables Docker scan suggestions
 export DOCKER_SCAN_SUGGEST=false
-
+#Sets the timeout for stopping the container to 10 seconds.
 stop_timeout=10
 need_build=false
 need_start=false
+#Store the first and second command-line arguments.
 option1="$1"
 option2="$2"
+#Causes the script to exit immediately if any command fails (useful for error handling).
 set -e;
-
-function echo_title {
+#print  -------------------
+       #STARTING CONTAINER
+       #-------------------
+echo_title() {
   line=$(echo "$1" | sed -r 's/./-/g')
   printf "\n$line\n$1\n$line\n\n"
 }
 
-function has_option {
-  if [ "$option1" == "$1" ] || [ "$option2" == "$1" ] ||
-     [ "$option1" == "$2" ] || [ "$option2" == "$2" ] ; then
+has_option() {
+  if [ "$option1" = "$1" ] || [ "$option2" = "$1" ] ||
+     [ "$option1" = "$2" ] || [ "$option2" = "$2" ] ; then
     echo "true"
   else
     echo "false"
@@ -23,14 +29,16 @@ function has_option {
 }
 # goto script directory
 pushd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" > /dev/null
-
+#Get Label TAG
 tag=$(cat Dockerfile | grep -oP 'cicd="\K\w+' | tail -1)
 if [ -z "$tag" ] ; then
   printf "\nNo cicd LABEL found in Dockerfile.\n\n"
   exit 1
 fi
 
-if [ $(has_option "--force" "-f") == "true" ] ; then
+# check if pull is needed
+# shellcheck disable=SC2046
+if [ $(has_option "--force" "-f") = "true" ] ; then
   need_pull=true
 else
   need_pull=$(git fetch --dry-run 2>&1)
@@ -57,9 +65,9 @@ elif [ -z $status ] ; then
   need_start=true
 fi
 
-if [ "$need_start" == false ] ; then
+if [ "$need_start" = false ] ; then
   printf "\nNo changes found. Container is already running.\n"
-elif [ "$need_build" == true ]; then
+elif [ "$need_build" = true ]; then
   echo_title "BUILDING & STARTING CONTAINER"
   docker-compose up -d --build
 else
@@ -67,10 +75,10 @@ else
   docker-compose up -d
 fi
 
-if [ $(has_option "--full_cleanup" "-fcu") == "true" ] ; then
+if [ $(has_option "--full_cleanup" "-fcu") = "true" ] ; then
   echo_title "FULL CLEAN-UP"
   docker image prune --force
-elif [ $(has_option "--cleanup" "-cu") == "true" ] ; then
+elif [ $(has_option "--cleanup" "-cu") = "true" ] ; then
   echo_title "CLEAN-UP"
   docker image prune --force --filter "label=cicd=$tag"
 fi
